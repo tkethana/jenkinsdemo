bff6475a380bbcd5bd648a4f245e478c23b3ac25
Tanmai <tanmai4a1@gmail.com>
2022-05-18 09:52:29 +0530

    hello commit for jenkins scm test

---
+++ HelloExample.java
@@ -0,0 +1,8 @@
+public class HelloExample {
+    public static void main(String[] s){
+        for(int i=0; i<=10; i++) {
+            System.out.println(i);
+            System.out.println("Done");
+        }
+    }
+}
